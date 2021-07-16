81d6b01c7ef98f1aecff184bd7574643e3f729f8
Luis Cabrera Benito <parzibyte@gmail.com>
2021-07-15 20:25:38 -0500

    Agregando iconos

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import '@mdi/font/css/materialdesignicons.css'
 import router from "./router"
 Vue.use(Buefy);
 Vue.config.productionTip = false
