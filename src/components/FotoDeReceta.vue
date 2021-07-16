fed80c7fe6966d2b343a0cbfd52f13379dd7be64
Luis Cabrera Benito <parzibyte@gmail.com>
2021-07-16 15:13:16 -0500

    Creando componente únicamente para la foto

---
+++ src/components/FotoDeReceta.vue
@@ -0,0 +1,17 @@
+<template>
+  <img :src="fotoDeReceta()" :alt="'Foto de ' + receta.nombre" />
+</template>
+<script>
+import RecetasService from "../services/RecetasService"
+export default {
+  props: ["receta"],
+  methods: {
+    fotoDeReceta() {
+      if (!this.receta.foto) {
+        return false;
+      }
+      return RecetasService.ubicacionFoto(this.receta.foto);
+    },
+  },
+};
+</script>
